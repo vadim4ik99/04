79031296456c9946e8c5c2223766af4e08a54bac
vadim4ik99 <47747472+vadim4ik99@users.noreply.github.com>
2021-07-28 16:04:58 +0300

    2
    2

--- t04_notes_with_cookies/js/script.js
+++ t04_notes_with_cookies/js/script.js
@@ -6,19 +6,23 @@ function addc(){
 	if(input !=""){
 		var cookie_date = new Date();
 		cookie_date.setMonth(cookie_date.getMonth() + 1);
-		document.cookie = "name=user;expires=" + cookie_date.toUTCString();
+		document.cookie = "notes="+input+";expires=" + cookie_date.toUTCString();
 
 	} else {alert("It\'s empty. Try to input something in \"Text input\"");}
 }
 
 function clearc(){
-	
+	let quest = confirm("Are you sure?");
+	if(quest) {
+
+	}
+	else return;
 }
 
 function getc() {
 	let arr = document.cookie.split(';');
 	let output = document.querySelector("#data");
-	if (arr.length > 0 !) {
+	if (arr.length > 0 ) {
 		arr.forEach( elm => { output.innerHTML = data; })
 		 
 	}
